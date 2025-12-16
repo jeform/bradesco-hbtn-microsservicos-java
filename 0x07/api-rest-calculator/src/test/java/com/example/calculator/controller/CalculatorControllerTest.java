58170b40e12d2d95f8d196d07eeb45413869e055
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 23:58:49 -0300

    refactor: add SpringExtension to CalculatorControllerTest for improved test context management

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -2,13 +2,16 @@ package com.example.calculator.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
     @Autowired
