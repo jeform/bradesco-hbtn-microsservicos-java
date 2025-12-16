9ce76bde4ecb4a37b994695dc98e1e37f861f2e7
Jesse Machado <jesse.machado@bradesco.com.br>
2025-12-15 23:47:48 -0300

    feat: implement User API with validation and exception handling

---
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.CPFException;
+import com.example.demo.exception.UserIdException;
+import com.example.demo.exception.UserNameException;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping("/user-id/{id}")
+    public String findUserById(@PathVariable int id) {
+        if (id > 0 && id < 100) {
+            return "You have entered valid ID";
+        } else {
+            throw new UserIdException(String.valueOf(id));
+        }
+    }
+
+    @GetMapping("/user-name/{userName}")
+    public String findUserByName(@PathVariable String userName) {
+        if (userName.length() > 3 && userName.length() < 15) {
+            return "You have entered valid USERNAME";
+        } else {
+            throw new UserNameException(userName);
+        }
+    }
+
+    @GetMapping("/user-cpf/{cpf}")
+    public String findUserByCPF(@PathVariable String cpf) {
+        boolean isCPFValid = isCPF(cpf);
+        if (isCPFValid) {
+            return "You have entered valid CPF";
+        } else {
+            throw new CPFException(cpf);
+        }
+    }
+
+    public boolean isCPF(String cpf) {
+        return cpf.length() > 3 && cpf.length() < 15;
+    }
+}
